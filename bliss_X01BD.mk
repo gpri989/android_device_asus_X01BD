eda788552a4aa7182ebfb2d91107925d8e47683d
Priyanshu Gupta <48028932+gpri989@users.noreply.github.com>
2020-08-28 20:55:12 +0530

    Bliss-ify

--- bliss_X01BD.mk
+++ bliss_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020 Havoc-OS
+# Copyright (C) 2019-2020 Bliss
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,16 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common RR stuff
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Official RR
-RR_BUILDTYPE := Official
 
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
+
+
+
+
 
 # Google Connectivity Services
 TARGET_INCLUDE_WIFI_EXT := true
@@ -38,7 +38,7 @@ TARGET_INCLUDE_WIFI_EXT := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := rr_X01BD
+PRODUCT_NAME := bliss_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA

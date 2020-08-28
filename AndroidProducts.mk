0ff8a835ba7ca1ecb0ee08009ffd35c95469c49a
Priyanshu Gupta <48028932+gpri989@users.noreply.github.com>
2020-08-28 21:04:15 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rr_X01BD.mk
+    $(LOCAL_DIR)/bliss_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    rr_X01BD-eng \
-    rr_X01BD-user \
-    rr_X01BD-userdebug
+    bliss_X01BD-eng \
+    bliss_X01BD-user \
+    bliss_X01BD-userdebug
